edbf167388f5f15013d3c2f0bdb453d19e1f3344
Jaxson Van Doorn <woofers1tt@gmail.com>
2023-09-09 16:53:00 -0700

    Init Next.js

---
+++ use-contact-demo/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'useContact',
+  description: '',
+} satisfies Metadata
+
+const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        <div id="__next">
+          <div className={inter.className}>{children}</div>
+        </div>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
