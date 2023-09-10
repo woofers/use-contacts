be251557bb53ef178c522b895603f4ce766ab218
Jaxson Van Doorn <woofers1tt@gmail.com>
2023-09-10 11:07:22 -0700

    Fix any

--- packages/use-contacts/lib/index.d.ts
+++ packages/use-contacts/lib/index.d.ts
@@ -6,13 +6,18 @@ type IsAny<T> = unknown extends T
   ? [keyof T] extends [never] ? false : true
   : false
 
+type DefinedContactKey<T extends ContactKey> = IsAny<T> extends true ? ContactKey : T
+
 type ContactWithFields<T extends ContactKey> = Contact<T> extends infer X ?
-    (IsAny<T> extends true ? { [K in ContactKey]: Contact<K>[K] } : { [K in keyof X]: Contact<T>[K] })
-    : never
+  { [K in keyof X]: Contact<T>[K] }
+  : never
+
+
 
 export declare const useContacts: (options?: ContactManagerOptions) => {
   getProperties: () => Promise<ContactKey[]>
-  select: <T extends ContactKey = ContactKey, K extends boolean = false>(properties?: T[] | undefined, options?: ContactOptions<K>) => Promise<K extends false ? [ContactWithFields<T>] : ContactWithFields<T>[]>
+  select: <T extends ContactKey = ContactKey, K extends boolean = false>(properties?: DefinedContactKey<T>[] | undefined, options?: ContactOptions<K>) => 
+    Promise<K extends false ? [ContactWithFields<DefinedContactKey<T>>] : ContactWithFields<DefinedContactKey<T>>[]>
   isSupported: () => boolean
   cancel: () => void
 }
