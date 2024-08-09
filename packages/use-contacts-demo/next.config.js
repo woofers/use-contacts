a89fbebbb928d46afbdc548c4d777b37894fd826
Jaxson Van Doorn <woofers1tt@gmail.com>
2024-08-09 05:46:56 +0000

    Use export on Next.js

--- packages/use-contacts-demo/next.config.js
+++ packages/use-contacts-demo/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    basePath: '/use-contacts',
+    assetPrefix: '/use-contacts/'
+}
 
-module.exports = nextConfig
+export default nextConfig
